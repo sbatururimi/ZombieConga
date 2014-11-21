ee0f36d09b270c4ad9076238436d7e9e95ea9c79
Stas Batururimi <blueocean87@me.com>
2014-11-21 12:38:28 +0300

    iteration 2

--- ZombieConga/Model/GameScene.swift
+++ ZombieConga/Model/GameScene.swift
@@ -9,6 +9,12 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let zombie: SKSpriteNode = SKSpriteNode(imageNamed: "zombie1")
+    var lastUpdateTime: NSTimeInterval = 0
+    var dt: NSTimeInterval = 0
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPointZero
+    
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
         let background = SKSpriteNode(imageNamed: "background1")
@@ -16,5 +22,35 @@ class GameScene: SKScene {
         
         background.position = CGPoint(x: size.width / 2, y: size.height / 2)
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        /// zombie
+        zombie.position = CGPoint(x: 400, y: 400)
+//        zombie.setScale(2.0)
+        addChild(zombie)
+        
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        if lastUpdateTime > 0
+        {
+            dt = currentTime - lastUpdateTime
+        }
+        else
+        {
+            dt = 0
+        }
+        lastUpdateTime = currentTime
+//        println("\(dt * 1000) milliseconds since last update")
+//        zombie.position = CGPoint (x: zombie.position.x + 4, y: zombie.position.y)
+        moveSprite(zombie, velocity: CGPoint(x: zombieMovePointsPerSec, y: 0))
+    }
+    
+    
+    
+    func moveSprite(sprite:SKSpriteNode, velocity: CGPoint)
+    {
+        let amountToMove = CGPoint(x:  velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        println("Amount to move: \(amountToMove)")
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
     }
 }
